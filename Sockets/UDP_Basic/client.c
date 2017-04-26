597e04dc52d0a675bd6fa6c1e7531c147a059e6b
Vijay A <vijayone1708@gmail.com>
2017-04-26 22:24:19 +0530

    Client

--- Sockets/UDP_Basic/client.c
+++ Sockets/UDP_Basic/client.c
@@ -1,13 +1,44 @@
 #include <stdio.h>         // printf
 #include <sys/socket.h>    // Sockets API
-#include <net/netinet.h>   // AF_INET
+#include <netinet/in.h>    // AF_INET
 #include <string.h>        // String APIs
 
-#define CLI_ADDR 10.144.172.187
+#define CLI_ADDR "10.144.172.187i"
 #define SIZE     50
 
 
-static void TX_DataToServer()
+static void TX_DataToServer(const int iSockfd, const struct sockaddr *pServAddr);
+
+
+static void TX_DataToServer(const int iSockfd, const struct sockaddr *pServAddr)
+{
+    int   size     = 0;
+    int   serv_len = sizeof(struct sockaddr_in);
+    char  sendbuff[SIZE] = {0};
+    char  recvbuff[SIZE] = {0};
+
+    while(1)
+    {
+        printf("%s Enter Input String to send:\n", __FILE__);
+
+        fgets(sendbuff, sizeof(sendbuff), stdin);
+
+        size = sendto(iSockfd, sendbuff, sizeof(sendbuff), 0, (struct sockaddr *)pServAddr, \
+                                                                           (socklen_t) serv_len);
+        {
+            perror("Sendto failed..");
+        }
+
+        size = recvfrom(iSockfd, recvbuff, sizeof(recvbuff), 0, (struct sockaddr *)pServAddr, \
+                                                                    (socklen_t *)&serv_len);
+        if(size < 0)
+        {
+            perror("recvfrom failed..");
+        }
+
+        printf("%s: Received from server is %s\n", __FILE__, recvbuff);
+    }
+}
 
 main()
 {
@@ -27,7 +58,7 @@ main()
 
     // Initialise the port and address values
     serv_addr.sin_family = AF_INET;
-    iRet = inet_pton(AF_INET, SERV_ADDR, &serv_addr.sin_addr);
+    iRet = inet_pton(AF_INET, CLI_ADDR, &serv_addr.sin_addr);
     if(iRet != 1)
     {
         perror("inet_pton failed..");
@@ -35,5 +66,6 @@ main()
 
     serv_addr.sin_port = htons(5000);
 
+    TX_DataToServer(iSockfd, (struct sockaddr *)&serv_addr);
      
 }
